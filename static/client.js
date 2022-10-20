1126dacf4ee3d7a282382585f5788b6cfd0a704f
zziyad <zziyadkhanov@gmail.com>
2022-10-20 11:39:40 +0400

    add example how to use api methods

--- static/client.js
+++ static/client.js
@@ -73,8 +73,44 @@ const scaffold = (url) => {
     },
     entity: {
       getEntity: ['name']
+    },
+    read: {
+      readFile: ['id']
     }
   });
-  const data = await api.entity.getEntity('post');
-  console.dir({ data });
+ 
+  
+
+const createForm = async (entity, id) => {
+  const schema = await api.entity.getEntity(entity);
+  const instance = await api.read.readFile(id);
+  console.log(schema);
+  const block = document.createElement('div');
+  block.setAttribute('class', 'block__row');
+  const inputs = {};
+  for (const field in schema[entity]) {
+    const definition = schema[entity][field]
+    const input = document.createElement('div');
+    input.setAttribute('class', field);
+    const blockContent = document.createElement('div');
+    blockContent.setAttribute('class', 'block__content');
+    const outPut = document.createElement(definition.control);
+    console.log({ definition });
+    if (definition.control === 'button') {
+      outPut.onclick = () => console.log('Hello from button');
+    }
+    outPut.innerHTML = instance[field];
+    blockContent.appendChild(outPut)
+    input.appendChild(blockContent);
+    block.appendChild(input);
+  }
+  // const button = document.createElement('button');
+  // button.innerHTML = 'Save';
+  // block.appendChild(button);
+  document.body.appendChild(block);
+
+
+}
+createForm('post', 3000);
+
 })();
