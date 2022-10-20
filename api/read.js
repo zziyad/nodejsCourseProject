1fa243f47423d37c35bfe4dd9abcbbedea616784
zziyad <zziyadkhanov@gmail.com>
2022-10-20 11:43:14 +0400

    modify read method

--- api/read.js
+++ api/read.js
@@ -1,7 +1,9 @@
 ({
  async readFile(id) {
-    const fileName = `./data/${parseInt(id)}.json`;
-    const data = await fs.promises.readFile(fileName, 'utf8');
-    // return JSON.parse(data);
+    const fileName = `/data/${parseInt(id)}.json`;
+    const filePath = node.process.cwd() + fileName
+    const data = await node.fs.promises.readFile(filePath, 'utf8');
+    // console.log({ data: JSON.parse(data) });
+    return JSON.parse(data);
   }
 })
